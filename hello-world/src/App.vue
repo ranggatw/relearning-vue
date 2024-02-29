d44bcce479d2bbd38ba0e19771144aeef87461c9
ranggatw <rangga.public@gmail.com>
2024-02-29 16:25:28 +0700

    using many way to binding classes

--- hello-world/src/App.vue
+++ hello-world/src/App.vue
@@ -1,15 +1,31 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h2 class="underline">This is text underline</h2>
+  <h2 v-bind:class="status">This is Binding Classes</h2>
+  <h2 class="underline" v-bind:class="status">This is Binding Classes and add class style</h2>
+  <hr />
+  <div v-bind:class="isPromoted && 'promoted'">This is Promoted</div>
+  <div v-bind:class="isSoldOut ? 'sold-out' : 'new' ">Sold out ? Movie</div>
+  <hr />
+  <div v-bind:class="['new', 'promoted']">Newly Promoted movie</div>
+  <div v-bind:class="[isPromoted && 'promoted', isSoldOut ? 'sold-out': 'new']">Array Conditional movie</div>
+  <div v-bind:class="{
+    promoted: isPromoted,  
+    new: !isPromoted,
+    'sold-out': isSoldOut
+  }">Object Conditional movie</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      status: 'danger',
+      isPromoted:  false,
+      isSoldOut: true
+    }
   }
 }
 </script>
@@ -23,4 +39,22 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+.underline {
+  text-decoration: underline;
+}
+
+.promoted {
+  color: green;
+  font-style: italic;
+}
+
+.new {
+  color: olivedrab;
+}
+
+.sold-out {
+   color: red;
+}
+
 </style>
